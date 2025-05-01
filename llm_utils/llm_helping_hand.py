348dae431498e4f2b25720d0301089aa216df0b6
Yugal Warade <yugalwarade.ai@gmail.com>
2025-05-02 01:40:24 +0530

    small change

--- llm_utils/llm_helping_hand.py
+++ llm_utils/llm_helping_hand.py
@@ -7,13 +7,13 @@ from pydantic import BaseModel,Field
 load_dotenv()
 
 
-class Structure_output(BaseModel):
-    """ LLM Output with Structure outoput as a valid terraform script"""
-    terraform_script = Field(description="Valid Terraform script with interndation and resourse allign without any error while execution")
+# class Structure_output(BaseModel):
+#     """ LLM Output with Structure outoput as a valid terraform script"""
+#     terraform_script = Field(description="Valid Terraform script with interndation and resourse allign without any error while execution")
 
 
 llm = ChatOpenAI()
-structure_llm = llm.with_structured_output(Structure_output)
+# structure_llm = llm.with_structured_output(Structure_output)
 def validate_input(prompt:str):
     if prompt:
         resp = llm.invoke(f"You are an expert in the Infrastructure as code domain. You are helping a user to create a terraform script for deploying  in the cloud. The user has selected the cloud provider. Validate if the given user has enterred the valid Terraform specific or not {prompt}, respond me in True or False only")
@@ -63,7 +63,7 @@ def query_enhancer(query: str, cloud: str, region: str, instance_type: str) -> s
     """
     prompt_template = ChatPromptTemplate.from_template(template_string)
     prompt = prompt_template.format_messages(query=query)
-    response = structure_llm.invoke(prompt)
+    response = llm.invoke(prompt)
     return response.content
 
 
